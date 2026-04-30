33d6d4a66410dcffbb442c0b07fb98fa3c5fb5b7
Holger Veltrup <veltrup@sitepark.com>
2026-04-30 09:50:06 +0200

    test: fix test

--- vips-ipc-worker/src/test/java/com/sitepark/vips/worker/command/CompareHandlerTest.java
+++ vips-ipc-worker/src/test/java/com/sitepark/vips/worker/command/CompareHandlerTest.java
@@ -9,18 +9,8 @@ import org.junit.jupiter.api.Test;
 class CompareHandlerTest {
   @Test
   void test() {
-    /*
     Path a = Path.of("src/test/resources/compare/a.png");
     Path b = Path.of("src/test/resources/compare/b.png");
-    */
-
-    Path a =
-        Path.of(
-            "/home/veltrup/git/imgfile/src/test/resources/references/ScaleWithBackgroundTest/scale-with-transparency-256x128-padding-fit/homer.png/256x128-padding-fit.png");
-    Path b =
-        Path.of(
-            "/home/veltrup/git/imgfile/target/test/images/ScaleWithBackgroundTest/scale-with-transparency-256x128-padding-fit/homer.png/256x128-padding-fit.png");
-
     Path result = Path.of("target/test-output/diff.png");
     CompareHandler handler = new CompareHandler();
     var compareResult = handler.handle(Compare.of(a, b, result, new Compare.Color(23, 77, 123)));
